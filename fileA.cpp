38a3ccc2c7042a1f74c5b3eb8271d705a97ee998
boyler-room <cdboyle@alaska.edu>
2017-09-21 14:58:10 -0800

    worx

--- fileA.cpp
+++ fileA.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "fileA.h"
+#include <cstdio>
 #include "file1.h"
 
 std::string fooA(std::string sA)
 {
+  if(sA=="") return "";
   std::cout<<sA<<"\n";
   std::string sX = "";
   for(int i=0;i<sA.size();i++){
-    sX+=std::string(1,sA[i])+"?";
+    sX+=std::string(1,sA[i])+"!";
     std::cout<<sX<<"\n";
-  }return fooZ(sX);
+  }if(sA.find('?',0)==sA.npos) return fooZ(sX);
+  else return sX;
 }
