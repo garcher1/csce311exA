77e302da402136b33c4938035e6d06e76fc14163
boyler-room <cdboyle@alaska.edu>
2017-09-21 13:50:16 -0800

    work

--- fileA.cpp
+++ fileA.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "fileA.h"
-//#include "file1.h"
+#include "file1.h"
 
 std::string fooA(std::string sA)
 {
-  return foo1(sA.substr(sA.size()-2)+"?";
+  std::cout<<sA<<"\n";
+  if(sA.size()==0) return "";
+  else if(sA.size()==1) return sA+"?";
+  return foo1(sA.substr(0,sA.size()-1)) + sA.substr(sA.size()-1) + "?";
 }
